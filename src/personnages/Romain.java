c662aa1cd3443a9326c8f0d47d59b73d6c72a756
Romain Luque <lqr3792a@ensfsi.onmicrosoft.com>
2025-09-30 18:52:26 +0200

    Ajout ToString dans Gaulois

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,24 @@
 package personnages;
 
 public class Romain {
-
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public int getForce() {
+		return force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+	private String prendreParole() {
+		return "Le romain" + nom + ":";
+	}
 }
