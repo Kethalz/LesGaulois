8a5971c3aa0f2e1ccf5b2eade3e20ba21200c042
Romain Luque <lqr3792a@ensfsi.onmicrosoft.com>
2025-10-21 12:47:34 +0200

    Fin invariant/Précond/Postcond

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 	public int getForce() {
 		return force;
@@ -23,15 +24,25 @@ public class Romain {
 	}
 	public void recevoirCoup(int forceCoup) {
 		// TODO Auto-generated method stub
+		int forceAvantCoup = force;
+		assert forceCoup>0;
 		force = force - forceCoup;
-		if (force<1) {
+		if (force<=0){
+			force=0;
 			parler("J'abandonne");
 		}
 		else {
 			parler("Aïe");
 		}
+		assert force<forceAvantCoup;
+		assert isInvariantVerified();
 	}
-	
+	private boolean isInvariantVerified() {
+		return getForce()>=0;
+	}
+	public static void main(String[] args) {
+		Romain minus = new Romain ("minus", 6);
+	}
 	
 
 }
